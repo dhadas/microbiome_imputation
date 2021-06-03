2b22d2674b2504a7f79cd32cd1ede1c478776855
dhadas <dhadas@gmail.com>
2021-06-03 18:05:22 +0300

    Added scripts for running via Condor.

--- condor_scripts/distributed_RF.py
+++ condor_scripts/distributed_RF.py
@@ -3,9 +3,11 @@ import sys
 
 if __name__ == '__main__':
 
+    print("In distributed_RF")
     if len(sys.argv) > 3:
         action = sys
         path_to_X, path_to_Y, path_to_folds_dict, curr_fold_num,  = sys.argv[1:]
+        print(sys.argv[1:])
         ignore_zeros_transformer = ignore_Y_zeros_transformer.get_transformer()
         preds_fold, rmse_fold = predict_species.init_fold_job(path_to_X, path_to_Y, path_to_folds_dict, curr_fold_num)
 
@@ -13,4 +15,5 @@ if __name__ == '__main__':
         rmse_fold.to_csv(f'rmse_fold_{curr_fold_num}.csv')
 
     else:
+        print("In distributed_RF, num of args <= 3")
         pass
